332fe05a758d39400ae2747c56de3d548b00d5df
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-10-09 13:02:18 +0100

    API working response displayed on emulator

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
@@ -19,7 +20,8 @@ class MainActivity : AppCompatActivity() {
 
     fun callJson() {
 
-        val url = "https://api.weatherbit.io/v2.0/current?city=Manchester&country=GB&key=30de3bdbeba044ed8eec5ae248945ba2"
+        val url =
+            "https://api.weatherbit.io/v2.0/current?city=Manchester&country=GB&key=30de3bdbeba044ed8eec5ae248945ba2"
         val request = Request.Builder().url(url).build()
 
         val client = OkHttpClient()
@@ -28,9 +30,13 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string()
                 println(body)
+
+                val gson = GsonBuilder().create()
+                val weather = gson.fromJson(body, Weather::class.java)
+                println(weather.data[0].city_name)
                 runOnUiThread {
                     findViewById<TextView>(R.id.api_display)
-                    api_display.text = body
+                    api_display.text = weather.data[0].city_name
                 }
 
             }
@@ -39,7 +45,57 @@ class MainActivity : AppCompatActivity() {
                 println("Nah, homie.")
             }
         })
-        }
-
-
     }
+
+}
+
+data class Weather(
+    val data: List<Datum>,
+    val count: Long
+)
+
+data class Datum(
+    val rh: Long,
+    val pod: String,
+    val lon: Double,
+    val pres: Float,
+    val timezone: String,
+    val obTime: String,
+    val country_code: String,
+    val clouds: Long,
+    val ts: Long,
+    val solarRAD: Double,
+    val stateCode: String,
+    val city_name: String,
+    val windSpd: Double,
+    val windCdirFull: String,
+    val windCdir: String,
+    val slp: Double,
+    val vis: Long,
+    val hAngle: Long,
+    val sunset: String,
+    val dni: Double,
+    val dewpt: Double,
+    val snow: Long,
+    val uv: Double,
+    val precip: Long,
+    val windDir: Long,
+    val sunrise: String,
+    val ghi: Double,
+    val dhi: Double,
+    val aqi: Long,
+    val lat: Double,
+    val weather: WeatherClass,
+    val datetime: String,
+    val temp: Double,
+    val station: String,
+    val elevAngle: Double,
+    val appTemp: Double
+)
+
+data class WeatherClass(
+    val icon: String,
+    val code: Long,
+    val description: String
+)
+
